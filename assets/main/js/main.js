22b5751f56dedd4c2dd16d2bbb0f2b34239940cb
developer00000000 <vohobovadilbar@gmail.com>
2024-12-27 10:11:40 +0500

    alert

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -1,37 +1,46 @@
-let TOKEN ='7362615665:AAEZRbtp_3tqhtRlzPub4Wvt1ZwkBMigK4U';
-let CHAT_ID= -1002496843835;
-let URL_API = `https://api.telegram.org/bot${ TOKEN}/sendMessage`;
+let TOKEN = '7362615665:AAEZRbtp_3tqhtRlzPub4Wvt1ZwkBMigK4U';
+let CHAT_ID = -1002496843835;
+let URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
+document.getElementById('tg').addEventListener('submit', function (e) {
+  e.preventDefault();
 
-document.getElementById('tg').addEventListener('submit',function(e){
-   e.preventDefault();
+  const form = this;
+  const name = form.name.value.trim();
+  const number = form.number.value.trim();
 
-   let message= `<b>Ro'yxat</b>\n`;
-   message+= `<b>Ism:</b> ${this.name.value}\n`;
-   message+= `<b>Raqam:</b> ${this.number.value}\n`;
+  if (!name || !number) {
+    alert("Iltimos, barcha maydonlarni to'ldiring!");
+    return;
+  }
 
+  let message = `<b>Ro'yxat</b>\n`;
+  message += `<b>Ism:</b> ${name}\n`;
+  message += `<b>Raqam:</b> +${number}\n`;
 
-   
-   console.log(message);
+  console.log(message);
 
+  axios
+    .post(URL_API, {
+      chat_id: CHAT_ID,
+      parse_mode: 'html',
+      text: message,
+    })
+    .then(() => {
+      form.name.value = '';
+      form.number.value = '';
 
-  axios.post(URL_API,{
-    chat_id:CHAT_ID,
-    parse_mode: 'html',
-    text: message
-  })
-  .then((res)=>{
-    this.name.value = '';
-    this.number.value = '';
+      alert("Ma'lumot muvaffaqiyatli yuborildi!");
+    })
+    .catch((err) => {
+      console.warn(err);
 
-  })
-  .catch(()=>{
-    console.warn(err)
-  })
-  .finally(()=>{
-   console.log('tugadi')
-  })
-})
+      alert("Xatolik yuz berdi. Iltimos, qayta urinib ko'ring.");
+    })
+    .finally(() => {
+      console.log('Tugadi');
+    });
+});
 
 
 
