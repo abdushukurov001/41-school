73143bc389bfeeb9f09f521d5ba2d32f2cb4656e
Rahmatxonov <rahmatxonov1001@gmail.com>
2024-12-28 21:31:24 +0500

    added file middleware and change settings file

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'about.middleware.Redirect404Middleware',
+
 ]
 
 ROOT_URLCONF = 'config.urls'
