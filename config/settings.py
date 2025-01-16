145dd9443a1d3bf220e88c204fd27618ea8c7d74
Asadbek <ahikmatullayev024@gmail.com>
2025-01-16 15:17:14 +0500

    [fix] config.settings.CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -30,9 +30,7 @@ DEBUG = True
 # ALLOWED_HOSTS = ['maktab-4.uz']
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 ALLOWED_HOSTS = ['*']
-
-
-
+CSRF_TRUSTED_ORIGINS = ['maktab-4.uz']
 # Application definition
 
 INSTALLED_APPS = [
